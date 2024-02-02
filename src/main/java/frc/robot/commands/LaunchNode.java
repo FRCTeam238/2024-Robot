d782e65d57de14730ba3b68dfb356a03c3d950c9
joe_Mother1212 <jojomojo1125@gmail.com>
2024-02-01 20:04:42 -0500

    Implement base commands for intake and launching
    Authored by David

---
+++ src/main/java/frc/robot/commands/LaunchNode.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Robot;
+
+public class LaunchNode extends Command {
+  /** Creates a new LaunchNode. */
+  public LaunchNode() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(Robot.feeder, Robot.shooter);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    Robot.feeder.rollerController(1);
+    Robot.shooter.setWheelTargetSpeed();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    Robot.feeder.rollerController(0);
+    Robot.shooter.setSpeed(0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
