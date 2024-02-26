70eafdf6c9d5b0a1d44f03b02d2972382025234d
Kevin-OConnor <koconnor@usfirst.org>
2024-02-26 02:12:15 -0500

    Shooter coast an current Limit

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -5,6 +5,7 @@ import static frc.robot.Constants.ShooterConstants.*;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -27,8 +28,11 @@ public class Shooter extends SubsystemBase implements Logged {
     config.Slot0.kI = kI;
     config.Slot0.kD = kD;
     config.Slot0.kV = kFF;
+    config.CurrentLimits.SupplyCurrentLimit = 30;
 
     rightMotor.setInverted(true);
+    rightMotor.setNeutralMode(NeutralModeValue.Coast);
+    leftMotor.setNeutralMode(NeutralModeValue.Coast);
     leftMotor.getConfigurator().apply(config);
     rightMotor.getConfigurator().apply(config);
 
