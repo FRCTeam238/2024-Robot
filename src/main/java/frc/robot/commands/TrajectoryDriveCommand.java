00b275857a180fe99fd85607e3efcdb033490485
Hayden Mitchell <hayd6262@gmail.com>
2024-03-07 20:56:32 -0500

    flip initial poses

--- src/main/java/frc/robot/commands/TrajectoryDriveCommand.java
+++ src/main/java/frc/robot/commands/TrajectoryDriveCommand.java
@@ -4,6 +4,7 @@ import com.choreo.lib.Choreo;
 import com.choreo.lib.ChoreoTrajectory;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
@@ -52,7 +53,9 @@ public class TrajectoryDriveCommand extends SequentialCommandGroup {
 
     // creates a command that sets the position of the robot to the starting point of the trajectory
     Command resetPos =
-        drivetrain.runOnce(() -> drivetrain.resetOdometry(trajectory.getInitialPose()));
+        drivetrain.runOnce(() -> drivetrain.resetOdometry(DriverStation.getAlliance().get() == DriverStation.Alliance.Red
+                ? trajectory.flipped().getInitialPose()
+                : trajectory.getInitialPose()));
     resetPos.addRequirements(drivetrain);
     if (resetPosition) {
       addCommands(resetPos);
